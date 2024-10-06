5cf111966a4713f027cbc740729af34d37677faf
CcNut0519 <wyn175720301@gmail.com>
2024-10-06 17:46:17 +0800

    新增readme

--- .vitepress/cache/deps/vitepress___@vueuse_core.js
+++ .vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -273,8 +273,8 @@ import {
   watchTriggerable,
   watchWithFilter,
   whenever
-} from "./chunk-CFHSMHJH.js";
-import "./chunk-6ZXFPDL5.js";
+} from "./chunk-PRYGE75I.js";
+import "./chunk-XIDTNVOX.js";
 export {
   DefaultMagicKeysAliasMap,
   StorageSerializers,
---
+++ README.md
@@ -0,0 +1,15 @@
+## 本地运行
+
+运行本项目首先fork本项目到自己的github仓库，然后克隆到本地：
+
+打开项目根目录，安装依赖：
+
+```bash
+pnpm add -D vitepress   # 安装vitepress依赖包
+```
+
+运行web服务器：
+
+```bash
+pnpm run docs:dev   # 启动本地热重载服务器
+```
